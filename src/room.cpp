9521696705e6fd82e5a9f014f0463e3b822649bc
payton.wu <payton.wu@outlook.com>
2025-04-01 23:30:46 +0800

    remove obsolete Room class implementation and update connect method to use FFI for connection setup

--- src/room.cpp
+++ src/room.cpp
@@ -22,9 +22,21 @@ auto Room::sid() const -> exec::task<std::string>
     co_return std::string{};
 }
 
-auto Room::connect(std::string_view url, std::string_view token, RoomOptions const & room_options) -> exec::task<void>
+auto Room::connect(std::string_view const url, std::string_view const token, RoomOptions const & room_options) -> exec::task<void>
 {
-    // Implementation of connect() method
+    proto::FfiRequest req;
+
+    auto * connection = req.mutable_connect();
+    connection->set_url(url.data());
+    connection->set_token(token.data());
+
+    auto * options = connection->mutable_options();
+    options->set_auto_subscribe(room_options.auto_subscribe);
+    options->set_dynacast(room_options.dynacast);
+
+    // TODO: e2ee options
+    // TODO: RTC config
+
     co_return;
 }
 
